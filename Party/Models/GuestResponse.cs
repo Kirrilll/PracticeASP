49f15c132158afaeebf9d62ce4b7dee5bb5e41c9
DESKTOP-FRTFPVS\User <k.larionov.02@mail.ru>
2022-05-04 23:51:18 +0300

    -add 7 practice

---
+++ Party/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите своё имя")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите свой email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Вы ввели некорректный email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пожалуйста, введите телефон")]
+        [RegularExpression("^\\+?[78][-\\(]?\\d{3}\\)?-?\\d{3}-?\\d{2}-?\\d{2}$", ErrorMessage = "Вы ввели некорректный номер телефона")]
+        public string Phone { get; set; }
+        public bool? IsAttend { get; set; }
+    }
+}
---
+++ Party/Repositories/PartyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using PartyInvites.Entities;
+
+namespace PartyInvites
+{
+    public class PartyRepository: DbContext
+    {
+        public DbSet<Guest> Guest { get; set; }
+    }
+}
