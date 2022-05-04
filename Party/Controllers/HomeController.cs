3702821735fbd9045bf45fb9f41a0a87028f6321
DESKTOP-FRTFPVS\User <k.larionov.02@mail.ru>
2022-05-05 01:52:58 +0300

    -add update; -add guests page;

--- Party/Controllers/HomeController.cs
+++ Party/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using PartyInvites.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,6 +11,7 @@ namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
+        PartyRepository _repository = new PartyRepository();
         // GET: Home
         public ViewResult Index()
         {
@@ -23,15 +25,31 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ViewResult Guests()
+        {
+            return View("Guests", _repository.Guest.ToList());
+        }
 
         [HttpPost]
         public ViewResult Form(GuestResponse guest)
         {
             if (ModelState.IsValid)
             {
-                PartyRepository repository = new PartyRepository();
-                repository.Guest.Add(new Guest(guest));
-                repository.SaveChanges();
+                var currGuest = _repository.Guest.FirstOrDefault<Guest>((g) => g.Phone.Equals(guest.Phone));
+                if (currGuest == null)
+                { 
+                    _repository.Guest.Add(new Guest(guest)); 
+                }
+                else
+                {
+                    currGuest.Name = guest.Name;
+                    currGuest.IsAttend = (bool)guest.IsAttend;
+                    currGuest.Email = guest.Email;
+                    _repository.Entry(currGuest).State = EntityState.Modified;
+                }
+              
+                _repository.SaveChanges();
                 return View("Feedback", guest);
             }
             else
